0caa494597b5434dedc004d0143fc0828997297a
Андрей Бредихин <102852334+andrewBredihin@users.noreply.github.com>
2022-07-03 10:59:04 +0400

    Настроил запрет на выбор User уже использующегося в Employee

--- src/main/java/com/company/var3/screen/employee/EmployeeEdit.java
+++ src/main/java/com/company/var3/screen/employee/EmployeeEdit.java
@@ -1,8 +1,12 @@
 package com.company.var3.screen.employee;
 
 import com.company.var3.app.EmployeeService;
+import com.company.var3.entity.User;
+import io.jmix.core.EntityStates;
 import io.jmix.ui.Dialogs;
 import io.jmix.ui.action.DialogAction;
+import io.jmix.ui.component.HasValue;
+import io.jmix.ui.component.ValuePicker;
 import io.jmix.ui.screen.*;
 import com.company.var3.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,21 +22,33 @@ public class EmployeeEdit extends StandardEditor<Employee> {
     @Autowired
     private Dialogs dialogs;
 
+    @Autowired
+    private EntityStates entityStates;
+
+    private User firstUser;
+
     @Subscribe
     public void onBeforeCommitChanges(BeforeCommitChangesEvent event) {
-        if (employeeService.checkForUser(getEditedEntity().getUser().getId())) {
-            dialogs.createOptionDialog()
-                    .withCaption("Message")
-                    .withMessage("Этот пользователь уже выбран")
-                    .withActions(
-                            new DialogAction(DialogAction.Type.OK).withHandler(e -> {
-                                getWindow().setFocusComponent("userField");
-                                event.preventCommit();
-                            })
-                    )
-                    .show();
+        if (entityStates.isNew(getEditedEntity()) || firstUser != getEditedEntity().getUser()){
+            if (employeeService.checkForUser(getEditedEntity().getUser().getId())) {
+                dialogs.createOptionDialog()
+                        .withCaption("Message")
+                        .withMessage("Этот пользователь уже выбран ")
+                        .withActions(
+                                new DialogAction(DialogAction.Type.OK).withHandler(e -> {
+                                    getWindow().setFocusComponent("userField");
+                                    event.preventCommit();
+                                })
+                        )
+                        .show();
 
-            event.preventCommit();
+                event.preventCommit();
+            }
         }
+    }
+
+    @Subscribe
+    public void onAfterShow(AfterShowEvent event) {
+        firstUser = getEditedEntity().getUser();
     }
 }
