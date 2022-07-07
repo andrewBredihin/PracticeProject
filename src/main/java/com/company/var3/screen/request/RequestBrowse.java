149033ebc1177f759d28ccf7cb3e2cb3fe25ee78
Андрей Бредихин <102852334+andrewBredihin@users.noreply.github.com>
2022-07-07 18:16:56 +0400

    Настроил фильтры Request - поиск заявок по инициатору

--- src/main/java/com/company/var3/screen/request/RequestBrowse.java
+++ src/main/java/com/company/var3/screen/request/RequestBrowse.java
@@ -1,10 +1,26 @@
 package com.company.var3.screen.request;
 
+import io.jmix.ui.component.HasValue;
+import io.jmix.ui.model.CollectionLoader;
 import io.jmix.ui.screen.*;
 import com.company.var3.entity.Request;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @UiController("Request.browse")
 @UiDescriptor("request-browse.xml")
 @LookupComponent("requestsTable")
 public class RequestBrowse extends StandardLookup<Request> {
+
+    @Autowired
+    private CollectionLoader<Request> requestsDl;
+
+    @Subscribe("initiatorNameField")
+    public void onInitiatorNameFieldValueChange(HasValue.ValueChangeEvent event) {
+        if (event.getValue() != null) {
+            requestsDl.setParameter("name", "(?i)%" + event.getValue() + "%");
+        } else {
+            requestsDl.removeParameter("name");
+        }
+        requestsDl.load();
+    }
 }
